587d67495ffc227fb0f16d8771cac358940680c4
Joe Bowman <joe@chorus.one>
2018-10-06 23:35:47 +0100

    handle wildcard / glob matches, and comma separated for module name, resource and attributes

--- _sdb/terraform.py
+++ _sdb/terraform.py
@@ -29,8 +29,13 @@ and ``public_ip`` is the key of the attribute for which you wish to return data.
 # import python libs
 from __future__ import absolute_import, print_function, unicode_literals
 import logging
+import os
 import json
+import hashlib
+import binascii
 import boto3
+import time
+import re
 import salt.exceptions
 
 LOG = logging.getLogger(__name__)
@@ -71,7 +76,7 @@ def get(key, profile=None):
 
 def notimplmented_backend():
     '''
-    If we try to define a backend that is unsupported, throwself.
+    If we try to define a backend that is unsupported, throw.
     '''
     raise salt.exceptions.NotImplemented()
 
@@ -80,11 +85,22 @@ def get_s3(key, profile):
     '''
     Fetch state file from s3 bucket. Currently only supports IAM roles,
     .aws/credentials files and env_vars.
-    TODO: support caching.
     '''
     s3_res = boto3.resource('s3')
-    s3_res.Object(profile.get('bucket'), profile.get('key')).download_file('/tmp/salt_tfstate')
-    return parse_tfstate_file(key, '/tmp/salt_tfstate')
+    hashfunc = hashlib.md5()
+    hashfunc.update(profile.get('key'))
+    hash = binascii.hexlify(hashfunc.digest())
+    if os.path.exists('/tmp/salt_tfstate_{}'.format(hash)) == False:
+        print ("Fetching tfstate from s3")
+        s3_res.Object(profile.get('bucket'), profile.get('key')).download_file('/tmp/salt_tfstate_{}'.format(hash))
+    else:
+        stat = os.stat('/tmp/salt_tfstate_{}'.format(hash))
+        if stat.st_mtime + profile.get('cache_duration', 60) < time.time():
+            print ("Cache is stale; refreshing from s3...")
+            s3_res.Object(profile.get('bucket'), profile.get('key')).download_file('/tmp/salt_tfstate_{}'.format(hash))
+        else:
+            print ("Using cache...")
+    return parse_tfstate_file(key, '/tmp/salt_tfstate_{}'.format(hash))
 
 
 def get_file(key, profile):
@@ -96,10 +112,9 @@ def get_file(key, profile):
 
 def parse_tfstate_file(full_key, file_path):
     '''
-    Parse the provided tfstate file, and search for the keyself.
-    TODO: support array values
-    TODO: support splat values
+    Parse the provided tfstate file, and search for the key.
     '''
+
     key, attr = full_key.split('/')
     key_parts = key.split('.')
 
@@ -114,14 +129,81 @@ def parse_tfstate_file(full_key, file_path):
     for k in modules_orig:
         data.get('modules').update({':'.join(k.get('path')): k})
 
+    result = parse_identifier(key_parts, attr, data)
+    if type(result) == 'string':
+        return result
+    else:
+        return json.dumps(result)
+
+
+def parse_identifier(key_parts, attr, data):
+    '''
+    Attempt to parse the given identifier. See README.md for more details.
+    '''
     if key_parts[0] == 'module':
-        ## we're checking for a module
-        mod_path = 'root:{}'.format(key_parts[1])
+        ## if we are module.* or module.host_*...then use regexp to gather all the possible modules.
+        if '*' in key_parts[1]:
+            pattern = re.compile(key_parts[1].replace('*','.+'))
+            mod_paths = [part for part in data.get('modules') if pattern.search(part)]
+        ## otherwise, just split by comma for the possible module paths i.e. module.host_saltmaster,host_dns...
+        else:
+            mod_paths = ['root:{}'.format(part) for part in key_parts[1].split(',')]
         key_parts = key_parts[2:]
     else:
-        mod_path = 'root'
+        ## no module, just the root.
+        mod_paths = ['root']
 
-    ## TODO: add support here for indexed and splat-style operators.
-    resource = data.get('modules').get(mod_path).get('resources').get('.'.join(key_parts))
+    if key_parts[0] == 'output':
+        ## we only care about outputs.
+        result = [data.get('modules', {}).get(p, {}).get('outputs', {}).get(key_parts[1], {}).get('value', '') for p in mod_paths]
+    else:
+        ## fetch one or more resources
+        result = [fetch_resource(key_parts, attr, data.get('modules', {}).get(p, {}).get('resources', {})) for p in mod_paths]
+    return flatten(result)
 
-    return resource.get('primary').get('attributes').get(attr)
+
+def flatten(result):
+    '''
+    Flatten any lists containing a single value.
+    '''
+    if type(result) in [list, tuple, set]:
+        if len(result) == 1:
+            return flatten(result[0])
+        else:
+            return [flatten(x) for x in result]
+    else:
+        return result
+
+
+def fetch_resource(key_parts, attr, data):
+    '''
+    Fetch the resource from a subset of data, given a reference.
+    '''
+    ### combine the key_parts, see we can check for the existence of wildcarc character or commas, which we handle differently.
+    resource_key = '.'.join(key_parts)
+
+    if '*' in resource_key:
+        ### if one or more wildcard chars are found, replace with .+ and return matches using regexp.
+        pattern = re.compile(resource_key.replace('*','.+'))
+        resources = [data.get(key) for key in data if pattern.search(key)]
+    elif ',' in resource_key:
+        ### if we have a comma (we only handle the first comma atm, because otherwise this gets insanely complex), find matches.
+        resources = []
+        split_part = [k for k in key_parts if ',' in k][0]
+        for key_part in split_part.split(','):
+            print(resource_key.replace(split_part, key_part))
+            resources.append(data.get(resource_key.replace(split_part, key_part)))
+    else:
+        ### otherwise, fetch the single resource matching the combined key_parts.
+        resources = [data.get(resource_key, {})]
+
+    if '*' in attr:
+        ### check the attr param for wildcard chars, and go regexp to find matches.
+        pattern = re.compile(attr.replace('*','.+'))
+        return [{k: resource.get('primary', {}).get('attributes', {}).get(k, None) for k in resource.get('primary', {}).get('attributes', {}) if pattern.match(k)} for resource in resources]
+    elif ',' in attr:
+        ### as above, handle comma separated attributes.
+        return [{k: resource.get('primary', {}).get('attributes', {}).get(k, None) for k in attr.split(',')} for resource in resources]
+    else:
+        ## otherwise return the full match.
+        return [resource.get('primary', {}).get('attributes', {}).get(attr, None) for resource in resources]
